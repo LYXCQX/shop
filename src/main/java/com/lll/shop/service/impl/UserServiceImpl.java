b15d8a8187b0eaf805569ea39cb53bbaa5bef4f9
king <liyixuan@monghoo.com>
2018-11-29 16:39:30 +0800

    commit

--- src/main/java/com/lll/shop/service/impl/UserServiceImpl.java
+++ src/main/java/com/lll/shop/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.alibaba.fastjson.JSON;
+import com.lll.shop.dao.GlobalconfigDao;
 import com.lll.shop.dao.UserDao;
 import com.lll.shop.dao.VerifyCodeDao;
 import com.lll.shop.pojo.BaseRes;
@@ -35,6 +36,9 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	private VerifyCodeDao verifyCodeDao;
+	
+	@Autowired
+	private GlobalconfigDao globalconfigDao;
 	/**
 	 * 用户注册
 	 */
@@ -134,7 +138,7 @@ public class UserServiceImpl implements UserService{
 	public BaseRes<UserRes> checkLogn(UserPojo userReq) {
 		BaseRes<UserRes> res = new BaseRes<UserRes>();
 		UserPojo user = userDao.selectUserByLoginId(userReq);
-		if (user != null && DateUtil.compare_date(DateUtil.getAfterTme(user.getLastLogInTime(), 30) , new Date())) {
+		if (user != null && DateUtil.compare_date(DateUtil.getAfterTme(user.getLastLogInTime(), Integer.valueOf(globalconfigDao.getGlobalByKey("loginExpire").getValue())) , new Date())) {
 			// 随便设置，数据库+1
 //			userReq.setLoginCount(1);
 			res.setData(toJava(user));
@@ -172,6 +176,8 @@ public class UserServiceImpl implements UserService{
 			res.setRes(ResCode.CODE_NO_SEND);
 		}else if (!userCode.getCode().equalsIgnoreCase(verifyCode.getCode())) {
 			res.setRes(ResCode.CODE_ERROR);
+		}else if(DateUtil.compare_date(DateUtil.getAfterTme(userCode.getCreateTime(), Integer.valueOf(globalconfigDao.getGlobalByKey("codeExpire").getValue())) , new Date())) {
+			
 		}
 		return res;
 	}
